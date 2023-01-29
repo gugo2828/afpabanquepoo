d1cf2b4464dfb039b2aae1b014f7d7509f5cd66e
etranger2008 <gugo2828@gmail.com>
2023-01-29 22:40:47 +0100

    Project 21,update

--- banque/src/main/java/org/example/Agence.java
+++ banque/src/main/java/org/example/Agence.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class Agence {
+    int id;
+    int code_agence;
+    String nom_agence;
+    String adresse;
 }
--- banque/src/main/java/org/example/Client.java
+++ banque/src/main/java/org/example/Client.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Client {
+
+    int id;
+    private String nom;
+    private String prenom;
+    private Date date_naissance;
+    private String email;
+
+   // private ArrayList<Agence> id_agence = new ArrayList<>();
+
+
 }
